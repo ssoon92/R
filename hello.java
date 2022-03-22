62916ddb6c1957e9228ed3e96bd9245779e34990
ssoon92 <jason1950@naver.com>
2022-03-22 17:21:28 +0900

    2022.03.22 연습

---
+++ hello.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Hello{
+
+public static void main(String args[])  {
+
+String str="*";
+
+for(int i=0;i<5;i++){
+System.out.print(str);
+str+="*";
+}
+
+//System.out.println("hello");
+
+
+}
+}
---
+++ html/Star.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Star{
+
+public static void main(String args[])  {
+
+String str="*";
+
+for(int i=0;i<5;i++){
+System.out.print(str);
+str+="*";
+System.out.println();
+}
+
+//System.out.println("hello");
+
+
+}
+}
